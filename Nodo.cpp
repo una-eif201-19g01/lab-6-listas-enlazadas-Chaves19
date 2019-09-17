fd81a4a0f9fac0ccbc1932e5cf4637728e58d0cb
ESCINF <ESCINF@LEN-M83>
2019-09-17 09:15:39 -0600

    listaLibro y Nodo casi listo

--- Nodo.cpp
+++ Nodo.cpp
@@ -2,4 +2,33 @@
 // Created by Maikol Guzman  on 2019-09-16.
 //
 
+
 #include "Nodo.h"
+
+Nodo::Nodo() {}
+
+Nodo::Nodo(const Libro &libro, Nodo *siguiente) : libro(libro), siguiente(siguiente) {}
+
+Nodo::~Nodo() {
+
+}
+
+const Libro &Nodo::getLibro() const {
+    return libro;
+}
+
+void Nodo::setLibro(const Libro &libro) {
+    Nodo::libro = libro;
+}
+
+Nodo *Nodo::getSiguiente() const {
+    return siguiente;
+}
+
+void Nodo::setSiguiente(Nodo *_siguiente) {
+    Nodo::siguiente = _siguiente;
+}
+
+std::string Nodo::toString() {
+    return libro->toString();
+}
