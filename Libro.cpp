70ca79bd0d2544a5e6fedda59f73d7e020992091
ESCINF <ESCINF@LEN-M83>
2019-09-17 09:28:22 -0600

    clase Libro lista

--- Libro.cpp
+++ Libro.cpp
@@ -3,3 +3,64 @@
 //
 
 #include "Libro.h"
+
+Libro::Libro() : nombre(""), editorial(""), anno(0), autor(""), id("") {
+
+}
+
+Libro::Libro(std::string nombre, std::string editorial, int anno, std::string autor, std::string id): nombre(nombre), editorial(editorial), anno(anno), autor(autor), id(id) {
+
+}
+
+Libro::~Libro() {
+
+}
+
+void Libro::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+std::string Libro::getNombre() {
+    return nombre;
+}
+
+void Libro::setEditorial(std::string editorial) {
+    this->editorial = editorial;
+}
+
+std::string Libro::getEditorial() {
+    return editorial;
+}
+
+void Libro::setAnno(int anno) {
+    this->anno = anno;
+}
+
+int Libro::getAnno() const {
+    return anno;
+}
+
+void Libro::setAutor(std::string autor) {
+    this->autor = autor;
+}
+
+std::string Libro::getAutor() {
+    return autor;
+}
+
+void Libro::setId(std::string id) {
+    this->id = id;
+}
+
+std::string Libro::getId() {
+    return id;
+}
+
+std::string Libro::toString() {
+
+}
+
+
+
+
+
--- Libro.h
+++ Libro.h
@@ -5,9 +5,28 @@
 #ifndef LAB_6_LISTAS_ENLAZADAS_LIBRO_H
 #define LAB_6_LISTAS_ENLAZADAS_LIBRO_H
 
-
 class Libro {
-
+private:
+    std::string nombre;
+    std::string editorial;
+    int anno;
+    std::string autor;
+    std::string id;
+public:
+    Libro();
+    Libro(std::string, std::string, int, std::string, std::string);
+    ~Libro();
+    void setAutor(std::string);
+    std::string getAutor();
+    void setAnno(int);
+    int getAnno();
+    void setEditorial(std::string);
+    std::string getEditorial();
+    void setNombre(std::string);
+    std::string getNombre();
+    void setId(std::string);
+    std::string getId();
+    std::string toString();
 };
 
 
